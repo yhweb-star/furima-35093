89da2f7f63c6b5b5574d07d9e3431eefe77fd1ed
yhweb-star <yh48121229@gmail.com>
2021-04-04 14:59:32 +0900

    itemコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :introduction, :category_id, :status_id, :postage_id, :shipping_area_id, :shipping_days_id, :price)
+  end
+  
 end
