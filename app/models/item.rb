3f1a8f18980785d87491a0b479a96151dd546b80
yhweb-star <yh48121229@gmail.com>
2021-04-04 14:44:57 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :shipping_area
+  belongs_to :shipping_days
+
 end
