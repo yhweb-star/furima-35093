e08e3e8087c618bab932b8599f1590b522600f8a
yhweb-star <yh48121229@gmail.com>
2021-04-04 14:50:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,6 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_days
 
+  validates :title, :introduction, :price, presence: true
+  validates :category_id, :status_id, :postage_id, :shipping_area_id, :shipping_days_id, numericality: { other_than: 1 } 
 end
