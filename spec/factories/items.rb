5fd77fca197f639f97916c1ff4935de406b19321
yhweb-star <yh48121229@gmail.com>
2021-04-05 14:59:11 +0900

    itemモデル単体テストコード用ファイルの配置

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title {Faker::Lorem.sentence}
+    introduction {Faker::Lorem.sentence}
+    category_id { 2 }
+    status_id { 3 }
+    postage_id { 2 }
+    shipping_area_id { 10 }
+    shipping_days_id { 3 }
+    price { 3000 }
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
