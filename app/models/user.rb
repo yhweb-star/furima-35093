e2d28ebad418fd55a3a6e26ffab2d9c867ac4bc5
yhweb-star <yh48121229@gmail.com>
2021-04-07 16:10:32 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_one :order, dependent: :destroy
 
   with_options presence: true do
     validates :nickname
